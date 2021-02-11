7d9b67ebeb5109ec42a0911662d281a4d8dd98cf
MathijsHofmeijer <mathijshofmeijer@gmail.com>
2021-02-11 14:51:37 +0100

    navbar bijna klaar

--- mainItems/header.php
+++ mainItems/header.php
@@ -4,11 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
-    <script src="../javascript/javascript.js"></script>
+    <link rel="stylesheet" href="./css/style.css">
+    <script defer src="./javascript/javascript.js"></script>
+    <script src="https://use.fontawesome.com/0296611000.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+          integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
     <title>Lekkerlezen.nl</title>
 </head>
 <body>
 <header>
-
+<div id="navbar">
+        <ul class="topnav">
+            <img id="logo" src="./images/unknown.png" alt="">
+            <li><b><a id="home" ><i class="fas fa-home"></i>  Home </a></b></li>
+            <li><b><a id="top10" ><i class="fas fa-list-ol"></i>  Top   10</a></b></li>
+            <li><b><a id="boeken" > <i class="fas fa-list"></i >  Boeken</a></b></li>
+            <img id="logo" src="./images/unknown.png" alt="">
+          </ul>
+    </div>
 </header>
