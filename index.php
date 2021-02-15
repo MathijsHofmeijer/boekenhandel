d2906ea9933b4a5eb15390eca3863e8d9ab5dfb0
MathijsHofmeijer <mathijshofmeijer@gmail.com>
2021-02-15 15:51:29 +0100

    schuinelement en begin top 3 boeken

--- index.php
+++ index.php
@@ -9,8 +9,31 @@
         <span class="letters letters-right">Lezen</span>
         <span class="line line2"></span>
     </span>
+    <div class="element-container">
+        <div class="schuinElement"></div>
+    </div>
     </h1> 
 <!-- intro tekst eind -->
+
+
+<!-- top 3 boeken -->
+<h2 id="top3"> De top 3 boeken </h2>
+    <!--top boek begin-->
+        <div id="top-boeken">
+            <div id="boek1">
+
+            </div>
+            <div id="boek2">
+
+            </div>
+            <div id="boek3">
+
+            </div>
+
+        </div>
+    <!-- top boek eind -->
+
+<!-- top 3 boeken eind -->
     
 </main>
 
