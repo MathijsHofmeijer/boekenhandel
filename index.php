27106728019b8cca20397a7fe0000bbb956037d5
MathijsHofmeijer <mathijshofmeijer@gmail.com>
2021-02-17 12:59:54 +0100

    bezig geweest met css

--- index.php
+++ index.php
@@ -18,10 +18,11 @@
 
 <!-- top 3 boeken -->
 <h2 id="top3"> De top 3 boeken </h2>
-    <!--top boek begin-->
+    <!--top boek begin  via waterval naar de 3 boeken --->
         <div id="top-boeken">
+           
             <div id="boek1">
-
+                
             </div>
             <div id="boek2">
 
@@ -29,11 +30,20 @@
             <div id="boek3">
 
             </div>
-
+            <img id="rivierimg" src="images/boekenrivier.jpg" alt="" srcset="">
+           
         </div>
     <!-- top boek eind -->
 
 <!-- top 3 boeken eind -->
+
+<!-- informatie over de winkel -->
+        <div id="winkelInformatie">
+            <div id='winkelinfo'> Informatie over ons!<br>
+            <p id="tekstoverons">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+        </div>
+<!-- informatie over de winkel end -->
     
 </main>
 
